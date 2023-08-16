173cde91ebd0c5d51e447b6f2553940f90d90092
Yesid Vanegas <yesidvanegas68@gmail.com>
2023-08-15 21:45:12 -0500

    Juego de triqui terminado con estadia de datos en sessionStorage

--- projects/02-juego-triqui/src/App.jsx
+++ projects/02-juego-triqui/src/App.jsx
@@ -1,26 +1,12 @@
 import { useState } from "react";
+import confetti from "canvas-confetti";
+import Square from "./Components/Square";
 
 const TURNS = {
   X: 'x',
   O: "o"
 }
 
-
-const Square = ({ children, isSelected, updateBoard, index }) => {
-
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-
-  const handleClick = () => {
-    updateBoard(index)
-  }
-
-  return (
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  )
-
-}
 
 const WINNER_COMBOS = [
   [0, 1, 3],
@@ -35,7 +21,12 @@ const WINNER_COMBOS = [
 
 function App() {
 
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
+    
   const [turn, setTurn] = useState(TURNS.X);
   const [winner, setWinner] = useState(null);
 
@@ -52,6 +43,10 @@ function App() {
     }
   }
 
+  const checkEndGame = (newBoard) => {
+    return newBoard.every((square) => square != null)
+   }
+
   const updateBoard = (index) => {
     if (board[index] || winner) return
     //actualizar board
@@ -61,19 +56,32 @@ function App() {
     //cambair turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn);
+    //Guardar partida
+    window.sessionStorage.setItem('board', JSON.stringify(newBoard))
+    window.sessionStorage.setItem('turn', newTurn)
     //Revisar si hay un ganador
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
+      confetti()
       setWinner(newWinner)
+    }else if(checkEndGame(newBoard)){
+      setWinner(false)
     }
   }
 
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+ 
 
 
 
   return (
     <main className="board">
       <h1>Triqui</h1>
+      <button onClick={resetGame}>Reset Game</button>
       <section className="game">
         {
           board.map((_, index) =>
@@ -93,6 +101,28 @@ function App() {
       <section className="turn">
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+
+        {
+          winner != null &&(
+            <section className="winner">
+              <div className="text">
+                <h2>
+                  {
+                    winner === false ? 'Empate' : 'Gano'
+                  }
+                </h2>
+
+                <header className="win">
+                  {winner && <Square>{winner}</Square>}
+                </header>
+
+                <footer>
+                  <button onClick={resetGame}>Empezar de nuevo</button>
+                </footer>
+              </div>
+            </section>
+          )
+        }
       </section>
     </main>
   )
